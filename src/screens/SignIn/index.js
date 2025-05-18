c30fa0526a84f5e5f9b0bfa15c211967c4037174
Phelipe Pereira <phelipescript@gmail.com>
2025-05-18 18:39:21 -0300

    add imagem do logo

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,8 +1,10 @@
+import { Image } from "react-native";
 import { Container, Title } from "./styles";
 
 export function SignIn() {
   return (
     <Container>
+      <Image source={require('../../../assets/logo.png')} />
       <Title>Tela de login</Title>
     </Container>
   )
