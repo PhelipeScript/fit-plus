c3272dfa74869768e82022ff37fe0fd6e5d7fe45
Phelipe Pereira <phelipescript@gmail.com>
2025-05-19 12:39:49 -0300

    add componente de input customizado

---
+++ src/components/CustomInput/index.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { Container, Input, InputWrapper, Label } from "./styles";
+import { EyeClosed } from "phosphor-react-native";
+import { TouchableOpacity } from "react-native";
+
+/**
+ * 
+ * @param {{
+  * label?: string, 
+  * placeholder: string, 
+  * value: string,
+  * secureTextEntry?: boolean,
+  * icon?: import('phosphor-react-native').Icon
+  * iconPos?: 'left' | 'right',
+  * separateIcon?: boolean,
+  * onChangeText?: () => void,
+  * onIconPress?: () => void,
+ * }} props 
+ */
+export function CustomInput({ 
+  label, 
+  placeholder, 
+  value = "",
+  secureTextEntry = false,
+  icon,
+  iconPos = 'left', 
+  separateIcon = false,
+  onChangeText, 
+  onIconPress,
+  ...props 
+}) {
+  const [isFocused, setIsFocused] = useState(false)
+
+  return (
+    <Container {...props}>
+      {label && <Label>{label}</Label>}
+      <InputWrapper isFocused={isFocused} iconPos={iconPos} separateIcon={separateIcon}>
+        {icon && onIconPress ? (
+          <TouchableOpacity onPress={onIconPress}>
+            {icon}
+          </TouchableOpacity>
+        ) : (
+          icon
+        )}
+
+        <Input 
+          placeholder={placeholder} 
+          value={value}
+          onChangeText={onChangeText}
+          onFocus={() => setIsFocused(true)} 
+          onBlur={() => setIsFocused(false)}
+          secureTextEntry={secureTextEntry}
+        />
+      </InputWrapper>
+    </Container>
+  )
+}
---
+++ src/components/CustomInput/styles.js
@@ -0,0 +1,38 @@
+import styled, { css } from "styled-components/native";
+
+export const Container = styled.View`
+  gap: 6px;
+  width: 100%;
+`;
+
+export const Label = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.fontFamily.regular};
+    font-size: ${theme.fontSizes.lg};
+    color: ${theme.colors.gray300};
+  `}
+`;
+
+export const InputWrapper = styled.View`
+  border-radius: 6px;
+  padding: 0px 16px;
+  align-items: center;
+  gap: 6px;
+
+  ${({ theme, isFocused, iconPos, separateIcon }) => css`
+    border: ${isFocused ? '2px solid ' + theme.colors.secondary : '1px solid '+ theme.colors.gray300};
+    justify-content: ${separateIcon ? 'space-between' : 'flex-start'};
+    flex-direction: ${iconPos === 'left' ? 'row' : 'row-reverse'};
+  `}
+`
+
+export const Input = styled.TextInput.attrs(({ theme }) => ({
+  placeholderTextColor: theme.colors.gray200,
+}))`
+  ${({ theme }) => css`
+    font-family: ${theme.fontFamily.regular};
+    font-size: ${theme.fontSizes.lg};
+    color: ${theme.colors.black};
+    flex: 1
+  `}
+`
