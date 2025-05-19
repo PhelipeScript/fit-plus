bcb7cd36f440ddd09e8e02520b60e8d5ffdcf406
Phelipe Pereira <phelipe.psouza@senacsp.edu.br>
2025-05-19 17:40:05 -0300

    finaliza o design da tela de cadastro

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,9 +1,86 @@
-import { Container, Title } from "./styles";
+import { Image, Keyboard, KeyboardAvoidingView, Platform, TouchableWithoutFeedback } from "react-native";
+import { Container, ContentWrapper, EyeClosedIcon, EyeOpenIcon, Form, SignInContainer, SignInText, Subtitle, Title, TitleContainer } from "./styles";
+import { CustomInput } from "../../components/CustomInput";
+import { useState } from "react";
+import { CustomButton } from "../../components/CustomButton";
+import { useNavigation } from "@react-navigation/native";
 
 export function SignUp() {
+  const navigate = useNavigation()
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+
+  function goToSignInScreen() {
+    navigate.replace('SignIn')
+  }
+
   return (
     <Container>
-      <Title>Tela de cadastro</Title>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'} 
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <ContentWrapper>
+            <Image source={require('../../../assets/logo.png')} />
+            <TitleContainer>
+              <Title>Criar Conta</Title>
+              <Subtitle>Preencha os campos abaixo para começar</Subtitle>
+            </TitleContainer>
+            
+            <Form>
+              <CustomInput 
+                label="Seu nome completo" 
+                placeholder="Digite o seu nome..." 
+                value={name}
+                onChangeText={setName}
+              />
+
+              <CustomInput 
+                label="Seu melhor email" 
+                placeholder="usuario@email.com" 
+                value={email}
+                onChangeText={setEmail}
+              />
+
+              <CustomInput 
+                label="Crie uma senha" 
+                placeholder="********" 
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={!showPassword}
+                icon={showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
+                onIconPress={() => setShowPassword(!showPassword)}
+                iconPos="right"
+                separateIcon
+              />
+
+              <CustomInput 
+                label="Confirme sua senha" 
+                placeholder="********" 
+                value={confirmPassword}
+                onChangeText={setConfirmPassword}
+                secureTextEntry={!showConfirmPassword}
+                icon={showConfirmPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
+                onIconPress={() => setShowConfirmPassword(!showConfirmPassword)}
+                iconPos="right"
+                separateIcon
+              />
+
+              <CustomButton title="Cadastrar" style={{ marginTop: 16 }} />
+            </Form>
+
+            <SignInContainer onPress={goToSignInScreen}>
+              <SignInText type="regular">Já tem uma conta?</SignInText>
+              <SignInText type="bold">Entre aqui</SignInText>
+            </SignInContainer>
+          </ContentWrapper>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </Container>
   )
 }
--- src/screens/SignUp/styles.js
+++ src/screens/SignUp/styles.js
@@ -1,14 +1,80 @@
-import styled from "styled-components/native";
+import { Eye, EyeClosed } from "phosphor-react-native";
+import { ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import styled, { css } from "styled-components/native";
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
+  background: ${props => props.theme.colors.background};
+`
+
+export const ContentWrapper = styled(ScrollView).attrs(({theme}) => ({
+  contentContainerStyle: {
+    flexGrow: 1,
+    alignItems: 'center',
+    backgroundColor: theme.colors.background,
+    padding: 24,
+  },
+  keyboardShouldPersistTaps: 'handled',
+}))``;
+
+export const TitleContainer = styled.View`
   align-items: center;
   justify-content: center;
-  background: ${props => props.theme.colors.background}
-`;
+  gap: 6px;
+  margin: 54px 0px 32px;
+`
 
 export const Title = styled.Text`
-  font-family: ${props => props.theme.fontFamily.regular};
-  font-size: ${props => props.theme.fontSizes['2xl']};
-  color: ${props => props.theme.colors.black};
-`;
+  ${({ theme }) => css`
+    font-family: ${theme.fontFamily.bold};
+    font-size: ${theme.fontSizes['2xl']};
+    color: ${theme.colors.secondary};
+  `}
+`
+
+export const Subtitle = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.fontFamily.regular};
+    font-size: ${theme.fontSizes.base};
+    color: ${theme.colors.gray300};
+  `}
+`
+
+export const Form = styled.View`
+  width: 100%;
+  gap: 16px;
+`
+
+export const EyeClosedIcon = styled(EyeClosed).attrs({
+  size: 24,
+})`
+  ${({ theme }) => css`
+    color: ${theme.colors.gray300};
+  `}
+`
+
+export const EyeOpenIcon = styled(Eye).attrs({
+  size: 24,
+})`
+  ${({ theme }) => css`
+    color: ${theme.colors.gray300};
+  `}
+`
+
+export const SignInContainer = styled.TouchableOpacity`
+  flex-direction: row;
+  gap: 6px;
+  align-items: center;
+  justify-content: center;
+`
+
+export const SignInText = styled.Text`
+  ${({ theme, type }) => css`
+    font-family: ${type === 'regular' ? theme.fontFamily.regular : theme.fontFamily.bold};
+    font-size: ${theme.fontSizes.base};
+    color: ${type === 'regular' ? theme.colors.gray300 : theme.colors.primary};
+  `}
+
+  margin-top: 48px;
+`
