1cb26e562098aa72209b305b4614d60c20759cdd
Phelipe Pereira <phelipescript@gmail.com>
2025-05-23 10:22:30 -0300

    estilizando bottom tabs, icones e cores

--- src/navigation/BottomTabsNavigation/index.js
+++ src/navigation/BottomTabsNavigation/index.js
@@ -1,16 +1,42 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Profile } from "../../screens/Profile";
 import { Workouts } from '../../screens/Workouts/index';
+import { Barbell, User } from "phosphor-react-native";
+import { useTheme } from "styled-components/native";
 
 const BottomTabs = createBottomTabNavigator()
 
 export function BottomTabsNavigation() {
+  const theme = useTheme()
+
   return (
     <BottomTabs.Navigator
       initialRouteName="Workouts"
-      screenOptions={{
+      screenOptions={({ route }) => ({
         headerShown: false,
-      }}
+        tabBarStyle: {
+          backgroundColor: theme.colors.background,
+        },
+        tabBarIcon: ({ focused }) => {
+          if (route.name === "Workouts") {
+            return (
+              <Barbell 
+                size={focused ? 28 : 24} 
+                color={focused ? theme.colors.primary : theme.colors.gray200} 
+                weight={focused ? "fill" : "regular"} 
+              />
+            );
+          } else if (route.name === "Profile") {
+            return (
+              <User 
+                size={focused ? 28 : 24} 
+                color={focused ? theme.colors.primary : theme.colors.gray200} 
+                weight={focused ? "fill" : "regular"} 
+              />
+            );
+          }
+        },
+      })}
     >
       <BottomTabs.Screen name="Workouts" component={Workouts} />
       <BottomTabs.Screen name="Profile" component={Profile} />
